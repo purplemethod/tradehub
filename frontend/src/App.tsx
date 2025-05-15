d58e54b8a5c11477d957a858664ec6b75a71c4a6
purplemethod <purple.method.consulting@gmail.com>
2025-05-15 16:41:27 -0300

    Added NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
+import NavBar from "./pages/NavBar";
+
 function App() {
   return (
     <>
-      <div className="bg-purple-500 text-white p-4">
-        <h1 className="text-3xl font-bold underline text-center text-black p-4">
-          Hello world!
-        </h1>
-      </div>
+      <NavBar />
     </>
   );
 }
